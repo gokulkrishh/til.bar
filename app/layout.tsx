947dd32bbd4534db577a820ece7fc74a23ead4f4
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2026-03-14 22:39:50 +0530

    feat(favicon): update favicon files and enhance icon structure for improved clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,9 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
       { url: "/icon.svg", type: "image/svg+xml", sizes: "any" },
     ],
     shortcut: "/favicon.ico",
