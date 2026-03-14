937d540e6488c76865f05193100869d79e45c01d
gokulkrishh <2944237+gokulkrishh@users.noreply.github.com>
2026-03-14 22:21:42 +0530

    fix(til-item-skeleton): adjust height for improved layout consistency

--- components/til-item-skeleton.tsx
+++ components/til-item-skeleton.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "./ui/spinner";
 
 export function TilItemSkeleton({ url }: { url: string }) {
   return (
-    <li className="flex h-10.25 items-center gap-4 py-1.5">
+    <li className="flex h-11.25 items-center gap-4 py-1.5">
       <div className="flex items-center gap-2 min-w-0 w-full">
         <Spinner className="size-3.5" />
         <span className="text-sm font-medium truncate animate-pulse">
